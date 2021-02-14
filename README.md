b8b6e67a3e928bd6325d8a740386f3878133ca9c
Abi-07 <Abhishekjoshi.2022@gmail.com>
2021-02-14 13:23:00 +0530

    Authentication and API integration added; prepare for heroku deployment

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # contact-book
+
+Full stack MERN application with user authentication and CRUD functionality.
+
+Register, login and manage your contacts online. Access from wherever you want, whichever device you are using.
+
+## Tech Stack:
+
+Node.js(express),
+MongoDB,
+React
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -25,6 +26,21 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState)
 
     // Load User
+    const loadUser = async () => {
+        if(localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+        try {
+            const res = await axios.get('/api/auth');
+
+            dispatch({ 
+                type: USER_LOADED, 
+                payload: res.data 
+            });
+        } catch (err) {
+            dispatch({ type: AUTH_ERROR });
+        }
+    }
 
     // Register User
     const register = async formData => {
@@ -41,6 +57,7 @@ const AuthState = props => {
                 type: REGISTER_SUCCESS,
                 payload: res.data
             });
+            loadUser();
         } catch (err) {
             dispatch({
                 type: REGISTER_FAIL,
@@ -50,10 +67,31 @@ const AuthState = props => {
     }
 
     // Login User
-    const login = () => console.log('login');
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/auth', formData, config);
+
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            });
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err.response.data.msg
+            });
+        }
+    }
 
     // Logout
-    const logout = () => console.log('logout');
+    const logout = () => dispatch({ type: LOGOUT });
 
     // Clear Errors
     const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
@@ -67,6 +105,7 @@ const AuthState = props => {
                 loading: state.loading,
                 user: state.user,
                 error: state.error,
+                loadUser,
                 register,
                 login,
                 logout,
