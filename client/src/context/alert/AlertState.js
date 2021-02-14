5811808deb8d929783f8edf3d2d693bd8147eded
Abi-07 <Abhishekjoshi.2022@gmail.com>
2021-02-14 14:17:42 +0530

    uuid package issue fixed

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid/v4';
+import uuid from 'uuid';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
 import {
@@ -14,7 +14,7 @@ const AlertState = props => {
 
     // Set Alert
     const setAlert = (msg, type, timeout = 5000) => {
-        const id = uuid;
+        const id = uuid.v4();
         dispatch({
             type: SET_ALERT,
             payload: { msg, type, id }
